9c1c0f336e10a3f957a68f1838b4acd0138c717e
Vadim L <lapov1011@gmail.com>
2024-08-19 13:14:47 +0600

    init

--- roles/vnc/files/vnc.ps1
+++
@@ -1,8 +0,0 @@
-# Stop all VNC processes at first
-Get-Process | ?{$_.Name -eq "vnc"} | Stop-Process -Force
-
-# If VNC exits, PowerShell will start it again.
-while ($true) {
-  Start-Process -FilePath "C:\Program Files\RealVNC\VNC Server\vncserver.exe" -Wait -WindowStyle Hidden
-  Start-Sleep 1
-}
